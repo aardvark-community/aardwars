c48b5888e3efe0c18bad8efd2393453dbbceb050
Sebastian Maierhofer <sebastian@maierhofers.net>
2022-07-11 17:06:45 +0200

    movespeed

--- src/Aardwars/Program.fs
+++ src/Aardwars/Program.fs
@@ -8,99 +8,55 @@ open Aardvark.Application
 open Aardvark.Application.Slim
 open Aardvark.Rendering.Text
 
-type Containedness =
-    | Outside
-    | On of Box.Flags
-    | Inside
+let statusTextSg (win : IRenderWindow) (t : aval<string>) (showText : aval<bool>) =
+    let textProj =
+        win.Sizes |> AVal.map (fun s ->
+            Trafo3d.Scale(float s.Y / float s.X, 1.0, 1.0)
+        )
+                        
+    let font = FontSquirrel.Anonymous_Pro.Regular
+    let leftTextSg =
+        let shape = 
+            t |> AVal.map (fun t -> font.Layout(C4b.White, t))
 
-let getContainedness (p : V3d) (b : Box3d) (eps : float) : Containedness = 
-    let bigger = Box3d.FromCenterAndSize(b.Center, b.Size + V3d.III * eps)
-    let smaller = Box3d.FromCenterAndSize(b.Center, b.Size - V3d.III * eps)
-    match smaller.Contains(p), bigger.Contains(p) with
-    | false, false -> Outside
-    | false, true -> 
-        let flag = 
-            let pp = (p - b.Center)
-            let m = pp.MajorDim
-            if abs pp.Z + eps >= abs pp.X && abs pp.Z + eps >= abs pp.Y then 
-                if pp.Z <= 0 then Box.Flags.MinZ else Box.Flags.MaxZ
-            elif m = 0 then if pp.X <= 0 then Box.Flags.MinX else Box.Flags.MaxX
-            elif m = 1 then if pp.Y <= 0 then Box.Flags.MinY else Box.Flags.MaxY
-            elif m = 2 then if pp.Z <= 0 then Box.Flags.MinZ else Box.Flags.MaxZ
-            else failwith "can not happen"
-        On flag
-    | true, true -> Inside
-    | true, false -> failwith "can never happen"
+        let trafo = 
+            (win.Sizes, shape) ||> AVal.map2 (fun s shape ->
+                let scale = 18.0 / float s.Y * 2.0
+                let bounds = Box2d(shape.bounds.Min * scale, shape.bounds.Max * scale)
+                let minX = -float s.X / float s.Y
+                let x = minX - bounds.Min.X + 0.02
+                let y = 1.0 - bounds.Max.Y - 0.02
 
-let updatePosition (p0 : V3d) (p1 : V3d) (b : Box3d) (eps : float) = 
-    let clampToFace face (p1 : V3d) =
-        match face with 
-        | Box.Flags.MinX -> 
-            V3d(b.Min.X, p1.Y, p1.Z), On face
-        | Box.Flags.MaxX -> 
-            V3d(b.Max.X, p1.Y, p1.Z), On face
-        | Box.Flags.MinY -> 
-            V3d(p1.X, b.Min.Y, p1.Z), On face
-        | Box.Flags.MaxY -> 
-            V3d(p1.X, b.Max.Y, p1.Z), On face
-        | Box.Flags.MinZ -> 
-            V3d(p1.X, p1.Y, b.Min.Z), On face
-        | Box.Flags.MaxZ -> 
-            V3d(p1.X, p1.Y, b.Max.Z), On face
-        | _ -> failwith "no face"
-    match getContainedness p0 b eps, getContainedness p1 b eps with 
+                Trafo3d.Scale(scale) *
+                Trafo3d.Translation(x, y, -1.0)
+            )
 
-    | On _, On face -> 
-        //printfn "ON %A" face
-        clampToFace face p1
-    | On face, Inside -> 
-        //printfn "OI %A" face
-        clampToFace face p1
-    | Outside, (Inside | On _) -> 
-        //let b = Box3d.FromCenterAndSize(b.Center, b.Size * 1.2)
-        let d = p1 - p0
-        let ray = Ray3d(p0,d)
-        let vMin, _ = b.GetMinMaxInDirection(d)
-        let px = Plane3d(V3d.IOO, vMin.X) 
-        let py = Plane3d(V3d.OIO, vMin.Y) 
-        let pz = Plane3d(V3d.OOI, vMin.Z) 
-        let hitPlane (dim : int) =
-            let p = if dim = 0 then px elif dim = 1 then py else pz
-            let mutable pt = 0.0
-            if ray.Intersects(p,&pt) then 
-                let phit = ray.GetPointOnRay pt
-                let containedInOther =
-                    if dim = 0 then
-                        b.Min.YZ.AllSmallerOrEqual(phit.YZ) &&
-                        b.Max.YZ.AllGreaterOrEqual(phit.YZ)
-                    elif dim = 1 then 
-                        b.Min.XZ.AllSmallerOrEqual(phit.XZ) &&
-                        b.Max.XZ.AllGreaterOrEqual(phit.XZ)
-                    else 
-                        b.Min.XY.AllSmallerOrEqual(phit.XY) &&
-                        b.Max.XY.AllGreaterOrEqual(phit.XY)
-                containedInOther
-            else 
-                false
-        
-        if hitPlane 0 then 
-            let face = if vMin.X = b.Min.X then Box.Flags.MinX else Box.Flags.MaxX
-            let newPos = V3d(vMin.X, p1.Y, p1.Z)
-            printfn "intersect %A -> %A (%A)" p1 newPos face
-            newPos, On face
-        elif hitPlane 1 then 
-            let face = if vMin.Y = b.Min.Y then Box.Flags.MinY else Box.Flags.MaxY
-            let newPos = V3d(p1.X, vMin.Y, p1.Z)
-            printfn "intersect %A -> %A (%A)" p1 newPos face
-            newPos, On face
-        else
-            let face = if vMin.Z = b.Min.Z then Box.Flags.MinZ else Box.Flags.MaxZ
-            let newPos = V3d(p1.X, p1.Y, vMin.Z)
-            printfn "intersect %A -> %A (%A)" p1 newPos face
-            newPos, On face
-    | _, Outside -> p1, Outside
-    | Inside, _ -> failwith "can never happen"
- 
+        Sg.shape shape
+        |> Sg.trafo trafo
+    //let rightTextSg =
+
+    //    let shape = 
+    //        statsText |> AVal.map (fun t -> font.Layout(C4b.White, t))
+
+    //    let trafo = 
+    //        (win.Sizes, shape) ||> AVal.map2 (fun s shape ->
+    //            let scale = 18.0 / float s.Y * 2.0
+    //            let bounds = Box2d(shape.bounds.Min * scale, shape.bounds.Max * scale)
+    //            let maxX = float s.X / float s.Y
+    //            let x = maxX - bounds.Max.X - 0.02
+    //            let y = 1.0 - bounds.Max.Y - 0.02
+    //            Trafo3d.Scale(scale) *
+    //            Trafo3d.Translation(x, y, -1.0)
+    //        )
+
+    //    Sg.shape shape
+    //    |> Sg.trafo trafo
+    Sg.ofList [leftTextSg]
+    |> Sg.onOff showText
+    |> Sg.viewTrafo (AVal.constant Trafo3d.Identity)
+    |> Sg.projTrafo textProj
+    |> Sg.depthTest' DepthTest.None
+    |> Sg.blendMode' BlendMode.Blend
 
 let repairMotion (p0 : V3d) (p1 : V3d) (b : Box3d) (r : float) =
     let d1 = 
@@ -110,7 +66,6 @@ let repairMotion (p0 : V3d) (p1 : V3d) (b : Box3d) (r : float) =
     if d1 > r then
         None
     else
-        // b.GetMinimalDistanceTo p0 >= r
         let dir = p1 - p0
         let (bMin, _) = b.GetMinMaxInDirection(dir)
 
@@ -118,18 +73,18 @@ let repairMotion (p0 : V3d) (p1 : V3d) (b : Box3d) (r : float) =
         let pt = p1.GetClosestPointOn(b)
         let d = Vec.distance pt p1
         if d <= r then
-
+            let e = 1E-1
             let x = 
-                if Fun.ApproximateEquals(pt.X, b.Max.X) then 1
-                elif Fun.ApproximateEquals(pt.X, b.Min.X) then -1
+                if Fun.ApproximateEquals(pt.X, b.Max.X,e) then 1
+                elif Fun.ApproximateEquals(pt.X, b.Min.X,e) then -1
                 else 0
             let y = 
-                if Fun.ApproximateEquals(pt.Y, b.Max.Y) then 1
-                elif Fun.ApproximateEquals(pt.Y, b.Min.Y) then -1
+                if Fun.ApproximateEquals(pt.Y, b.Max.Y,e) then 1
+                elif Fun.ApproximateEquals(pt.Y, b.Min.Y,e) then -1
                 else 0
             let z = 
-                if Fun.ApproximateEquals(pt.Z, b.Max.Z) then 1
-                elif Fun.ApproximateEquals(pt.Z, b.Min.Z) then -1
+                if Fun.ApproximateEquals(pt.Z, b.Max.Z,e) then 1
+                elif Fun.ApproximateEquals(pt.Z, b.Min.Z,e) then -1
                 else 0
 
             let inline repairX() =
@@ -150,110 +105,25 @@ let repairMotion (p0 : V3d) (p1 : V3d) (b : Box3d) (r : float) =
             elif x = 0 && y = 0 && z <> 0 then repairZ()
 
             elif x <> 0 && y <> 0 && z = 0 then
-                if (p0.XY - pt.XY).MajorDim = 0 then repairX()
+                if (p0.XY - pt.XY).Abs().MajorDim = 0 then repairX()
                 else repairY()
                     
             elif x <> 0 && y = 0 && z <> 0 then
-                if (p0.XZ - pt.XZ).MajorDim = 0 then repairX()
+                if (p0.XZ - pt.XZ).Abs().MajorDim = 0 then repairX()
                 else repairZ()
                 
             elif x = 0 && y <> 0 && z <> 0 then
-                if (p0.YZ - pt.YZ).MajorDim = 0 then repairY()
+                if (p0.YZ - pt.YZ).Abs().MajorDim = 0 then repairY()
                 else repairZ()
 
             else
-                match (p0 - pt).MajorDim with
+                match (p0 - pt).Abs().MajorDim with
                 | 0 -> repairX()
                 | 1 -> repairY()
                 | _ -> repairZ()
 
         else
             None
-
-        //let minX = Fun.ApproximateEquals(pt.X, b.Min.X)
-
-
-
-        //let px = Plane3d(V3d.IOO, bMin.X)
-        //let py = Plane3d(V3d.OIO, bMin.Y)
-        //let pz = Plane3d(V3d.OOI, bMin.Z)
-
-        //let hx = px.Height(p1)
-        //let hy = py.Height(p1)
-        //let hz = pz.Height(p1)
-
-        //let xc = abs hx < r
-        //let yc = abs hy < r
-        //let zc = abs hz < r
-
-        //match xc,yc,zc with
-        //| true, false, false -> 
-        //    let res =
-        //        if hx < r then p1 + px.Normal * (r - hx)
-        //        else p1 + px.Normal * (hx - r)
-        //    Some res
-
-        //| false, true, false -> 
-        //    let res =
-        //        if hy < r then p1 + py.Normal * (r - hy)
-        //        else p1 + py.Normal * (hy - r)
-        //    Some res
-
-        //| false, false, true -> 
-        //    let res =
-        //        if hz < r then p1 + pz.Normal * (r - hz)
-        //        else p1 + pz.Normal * (hz - r)
-        //    Some res
-
-        //| true, true, false  -> 
-        //    let h0 = px.Height p0 |> abs
-        //    let h1 = py.Height p0 |> abs
-        //    if h0 > h1 then
-        //        let res =
-        //            if hx < r then p1 + px.Normal * (r - hx)
-        //            else p1 + px.Normal * (hx - r)
-        //        Some res
-        //    else 
-        //        let res =
-        //            if hy < r then p1 + py.Normal * (r - hy)
-        //            else p1 + py.Normal * (hy - r)
-        //        Some res
-                
-        //| true, false, true  -> 
-        //    let h0 = py.Height p0 |> abs
-        //    let h1 = pz.Height p0 |> abs
-        //    if h0 > h1 then
-        //        let res =
-        //            if hy < r then p1 + py.Normal * (r - hy)
-        //            else p1 + py.Normal * (hy - r)
-        //        Some res
-        //    else 
-        //        let res =
-        //            if hz < r then p1 + pz.Normal * (r - hz)
-        //            else p1 + pz.Normal * (hz - r)
-        //        Some res
-                
-        //| false, true, true  ->
-        //    let h0 = px.Height p0 |> abs
-        //    let h1 = pz.Height p0 |> abs
-        //    if h0 > h1 then
-        //        let res =
-        //            if hx < r then p1 + px.Normal * (r - hx)
-        //            else p1 + px.Normal * (hx - r)
-        //        Some res
-        //    else 
-        //        let res =
-        //            if hz < r then p1 + pz.Normal * (r - hz)
-        //            else p1 + pz.Normal * (hz - r)
-        //        Some res
-        //| true, true, true   -> failwith "corner"
-        //| false,false, false -> failwith "can not happen"
-
-
-
-
-
-
 
 [<EntryPoint>]
 let main (_args : string[]) =
@@ -263,18 +133,18 @@ let main (_args : string[]) =
 
     let camera = cval (CameraView.lookAt (V3d(0,0,50)) (V3d(0,100,2)) V3d.OOI) 
     let keyboard = win.Keyboard
-    let movespeed = 15.0
-    let gravity = 1.5
-    let jumpspeed = 0.8
-    let playerHeight = 1.85
-
+    let movespeed = 5.0
+    let gravity = 1.725
+    let jumpHeight = 6.95 
+    let airAccel = 0.025
+    let playerHeight = 1.35
 
     let glfw = win.GetType().GetField("glfw", BindingFlags.NonPublic ||| BindingFlags.Instance).GetValue(win) :?> Silk.NET.GLFW.Glfw
     let hwin = win.GetType().GetField("win", BindingFlags.NonPublic ||| BindingFlags.Instance).GetValue(win) :?> nativeptr<Silk.NET.GLFW.WindowHandle>
     let mutable mouseDelta = V2d.Zero
 
     let mutable v = V3d.Zero
-    win.Fullcreen <- true
+    win.Fullcreen <- false
     win.Cursor <- Cursor.None
     win.VSync <- true
     
@@ -291,17 +161,19 @@ let main (_args : string[]) =
     let mutable velocity = V3d.Zero
     let mutable onFloor = true
      
+    win.Keyboard.DownWithRepeats.Values.Add (fun k -> 
+        match k with 
+        | Keys.Space -> moveVelocity.Z <- jumpHeight
+        | _ -> ()
+    )
+
     win.Keyboard.Down.Values.Add (fun k ->
         match k with
         | Keys.Escape -> win.Close()
         | Keys.W -> moveVelocity.Y <- moveVelocity.Y + movespeed
         | Keys.S -> moveVelocity.Y <- moveVelocity.Y - movespeed
-        | Keys.Space -> moveVelocity.Z <- moveVelocity.Z + movespeed * jumpspeed
         | Keys.A -> moveVelocity.X <- moveVelocity.X - 0.9 * movespeed
         | Keys.D -> moveVelocity.X <- moveVelocity.X + 0.9 * movespeed
-        //| Keys.LeftShift -> moveVelocity.XY <- moveVelocity.XY + 5.0
-        
-             
         | _ -> ()
     )
     win.Keyboard.Up.Values.Add (fun k ->
@@ -310,8 +182,7 @@ let main (_args : string[]) =
         | Keys.S -> moveVelocity.Y <- moveVelocity.Y + movespeed
         | Keys.A -> moveVelocity.X <- moveVelocity.X + 0.9 * movespeed
         | Keys.D -> moveVelocity.X <- moveVelocity.X - 0.9 * movespeed
-        
-            
+        | Keys.Space -> moveVelocity.Z <- 0.0
         | _ -> ()
     )
     
@@ -333,14 +204,17 @@ let main (_args : string[]) =
                         Trafo3d.Translation(float x, float y, float z) *
                         Trafo3d.Translation(V3d(-offset, 0.0))
         |]
-    let trafos = testScene (V2i(100, 100)) 1000
+    Log.startTimed "Create lvl"
+    let trafos = testScene (V2i(100, 100)) 2000
     let worldBoxes = 
         trafos |> Array.map (fun t -> 
             let b = Box3d.Unit.Transformed(t)
             //Box3d.FromCenterAndSize(b.Center, b.Size + V3d.III * 0.01)
             b
         )
+    Log.stop()
     
+    let displayVelo = cval V3d.Zero
     let sw = System.Diagnostics.Stopwatch.StartNew()
     let thread = 
         startThread <| fun () ->
@@ -384,40 +258,43 @@ let main (_args : string[]) =
                     )   
                 
                     if onFloor then 
-                        velocity <- moveVelocity
+                        //inertia on ground
+                        let velo = Fun.Lerp(0.12, velocity.XY, moveVelocity.XY)
+                        velocity <- V3d(velo.X, velo.Y, moveVelocity.Z)
                     else
-                        moveVelocity.Z <- 0.0
-                        let geschwindigkeit = velocity.Z - gravity * 9.8076 * dt
-                        //let relativeBewegung = x + geschw * dt
-                        //let geschwindigkeit = velocity.Z
+                        //gravity
+                        let geschwindigkeit = velocity.Z - gravity * 9.8076 * dt 
                         velocity.Z <- geschwindigkeit
+                        //air control
+                        velocity.XY <- velocity.XY + moveVelocity.XY.Normalized * airAccel
 
                     let fw = camera.Value.Forward.XYO |> Vec.normalize
                     let r = camera.Value.Right.XYO |> Vec.normalize
+                    //position change in timestep dt
                     let delta = ((fw * velocity.Y + r * velocity.X + V3d.OOI * velocity.Z) * dt)
                     if not (delta |> Fun.IsTiny) then 
+                        //collision with box/floor detection and repair position
                         let p0 = camera.Value.Location - (V3d.OOI * playerHeight)
                         let mutable p1 = (camera.Value.Location + delta) - (V3d.OOI * playerHeight)
                         let mutable zUp = false
-                        let (p1) =
-                            let res = 
-                                worldBoxes 
-                                |> Array.fold (fun p1 b ->
-                                    let pos = repairMotion p0 p1 b 0.2
-                                    match pos with
-                                    | Some (z, p) -> 
-                                        if z then zUp <- true
-                                        p
-                                    | None -> p1
-                                ) p1
-                            res
+                        let tryRepair p1 =
+                            worldBoxes 
+                            |> Array.fold (fun p1 b ->
+                                let pos = repairMotion p0 p1 b 0.2
+                                match pos with
+                                | Some (z, p) -> 
+                                    if z then zUp <- true
+                                    p
+                                | None -> p1
+                            ) p1
+                        let p1 = tryRepair p1
 
                         moveTo (p1 + (V3d.OOI * playerHeight))
 
                         if zUp then onFloor <- true
-                        else
-                            onFloor <- camera.Value.Location.Z <= playerHeight + 0.01 //|| (contained = On Box.Flags.MaxZ)
+                        else onFloor <- camera.Value.Location.Z <= playerHeight //|| (contained = On Box.Flags.MaxZ)
                 
+                    transact (fun _ -> displayVelo.Value <- velocity)
                     mm.Wait()
 
     let cfg : TextConfig =
@@ -490,8 +367,13 @@ let main (_args : string[]) =
         |> Sg.viewTrafo realPlayerTrafo
         |> Sg.projTrafo (win.Sizes |> AVal.map (fun s -> Frustum.perspective 90.0 0.2 10000.0 (float s.X / float s.Y) |> Frustum.projTrafo))
 
+    let statusText =
+        displayVelo |> AVal.map (fun v -> sprintf "%.2f" v.Length)
+    let textSg = statusTextSg win statusText (AVal.constant true)
+    let finalSg =
+        Sg.ofList [scene; textSg]
     win.RenderTask <- 
-        Sg.compile' win.Runtime win.FramebufferSignature false scene
+        Sg.compile' win.Runtime win.FramebufferSignature false finalSg
     
     win.Focus()
     win.Run()
