bb54bfc7528331229c8697a0c4f2d384a8d89b11
Georg Haaser <krauthaufen@awx.at>
2022-07-13 11:57:48 +0200

    ELM

---
+++ src/Aardwars/Model.fs
@@ -0,0 +1,43 @@
+﻿namespace Elm
+
+open FSharp.Data.Adaptive
+open Adaptify
+open Aardvark.Rendering
+open Aardvark.Base
+
+
+type World =
+    abstract Hit : p0 : V3d * p1 : V3d -> V3d * bool
+
+
+type PlaneWorld(z : float) =
+    interface World with
+        member x.Hit(p0 : V3d, p1 : V3d) =
+            let mutable p1 = p1
+            if p1.Z <= z then
+                p1.Z <- z
+                p1, true
+            else
+                p1, false
+
+[<ModelType>]
+type CameraModel =
+    {
+        look        : bool
+        move        : V3d
+        velocity    : V3d
+        camera      : CameraView
+    }
+
+[<ModelType>]
+type Model =
+    {
+        world       : World
+        onFloor     : bool
+        time        : float
+        size        : V2i
+        camera      : CameraModel
+        proj        : Frustum
+    }
+
+
