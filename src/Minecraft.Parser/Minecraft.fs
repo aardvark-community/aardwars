127bd3983869e7fdf146bd0e0ece882ccb3ff00a
Stefan Maierhofer <sm@stefanmaierhofer.com>
2022-07-13 08:45:37 +0200

    parser for minecraft maps (wip)

--- src/Minecraft.Parser/Minecraft.fs
+++ src/Minecraft.Parser/Minecraft.fs
@@ -3,6 +3,7 @@
 open System.IO
 open ICSharpCode.SharpZipLib.Zip.Compression.Streams
 open System.Text
+open System
 
 module Minecraft =
     
@@ -17,7 +18,7 @@ module Minecraft =
     
     type NbtPayload =
         | PayloadEnd                        // 0
-        | PayloadByte of int8               // 1
+        | PayloadByte of byte               // 1
         | PayloadShort of int16             // 2
         | PayloadInt of int32               // 3
         | PayloadLong of int64              // 4
@@ -32,14 +33,24 @@ module Minecraft =
     and 
         NbtTag = { Name : string; Payload : NbtPayload }
 
-    let readBigEndianInt16 (xs : byte[]) o : int16 = (int16(xs.[o  ]) <<<  8) ||| (int16(xs.[o+1])       )
-    let readBigEndianInt32 (xs : byte[]) o : int32 = (int32(xs.[o  ]) <<< 24) ||| (int32(xs.[o+1]) <<< 16) ||| (int32(xs.[o+2]) <<<  8) ||| (int32(xs.[o+3])       )
-    let readBigEndianInt64 (xs : byte[]) o : int64 = (int64(xs.[o  ]) <<< 56) ||| (int64(xs.[o+1]) <<< 48) ||| (int64(xs.[o+2]) <<< 40) ||| (int64(xs.[o+3]) <<< 32) |||
-                                                     (int64(xs.[o+4]) <<< 24) ||| (int64(xs.[o+5]) <<< 16) ||| (int64(xs.[o+6]) <<<  8) ||| (int64(xs.[o+7])       )
+    let parseInt16 (xs : byte[]) o : int16 = (int16(xs.[o  ]) <<<  8) ||| (int16(xs.[o+1])       )
+    let parseInt24 (xs : byte[]) o : int32 = (int32(xs.[o  ]) <<< 16) ||| (int32(xs.[o+1]) <<<  8) ||| (int32(xs.[o+2])       )
+    let parseInt32 (xs : byte[]) o : int32 = (int32(xs.[o  ]) <<< 24) ||| (int32(xs.[o+1]) <<< 16) ||| (int32(xs.[o+2]) <<<  8) ||| (int32(xs.[o+3])       )
+    let parseInt64 (xs : byte[]) o : int64 = (int64(xs.[o  ]) <<< 56) ||| (int64(xs.[o+1]) <<< 48) ||| (int64(xs.[o+2]) <<< 40) ||| (int64(xs.[o+3]) <<< 32) |||
+                                             (int64(xs.[o+4]) <<< 24) ||| (int64(xs.[o+5]) <<< 16) ||| (int64(xs.[o+6]) <<<  8) ||| (int64(xs.[o+7])       )
+    
+    let parseFloat32 (xs : byte[]) o : float32 =
+        [| xs.[o + 3]; xs.[o + 2]; xs.[o + 1]; xs.[o    ] |]
+        |> BitConverter.ToSingle
 
-    let readBigEndian16At (xs : byte[]) o : int = (int(xs.[o]) <<<  8) ||| (int(xs.[o+1])       )
-    let readBigEndian24At (xs : byte[]) o : int = (int(xs.[o]) <<< 16) ||| (int(xs.[o+1]) <<< 8 ) ||| (int(xs.[o+2])      )
-    let readBigEndian32At (xs : byte[]) o : int = (int(xs.[o]) <<< 24) ||| (int(xs.[o+1]) <<< 16) ||| (int(xs.[o+2]) <<< 8) ||| int(xs.[o+3])
+    let parseFloat64 (xs : byte[]) o : float =
+        [| xs.[o + 7]; xs.[o + 6]; xs.[o + 5]; xs.[o + 4]; xs.[o + 3]; xs.[o + 2]; xs.[o + 1]; xs.[o    ] |]
+        |> BitConverter.ToDouble
+    
+    let parseString xs o =
+        let tagNameLength = int(parseInt16 xs o)
+        let tagName = Array.sub xs (o + 2) tagNameLength |> Encoding.UTF8.GetString
+        (tagName, o + 2 + tagNameLength)
 
     /// Returns all regions in given minecraft map directory.
     let getRegions minecraftMapDir =
@@ -68,15 +79,15 @@ module Minecraft =
 
         seq {
             let xs = File.ReadAllBytes region.FileName
-            printfn "file size: %d" xs.Length
+            printfn "%s [%d]" region.FileName xs.Length
             for z = 0 to 31 do
                 for x = 0 to 31 do
-                    let o = (offset x z |> readBigEndian24At xs) <<< 12
+                    let o = (offset x z |> parseInt24 xs) <<< 12
                     match o with
                     | 0 -> () // chunk (x, z) does not exist
                     | _ ->
                         let blockCount4k = int32(xs.[o+3]) <<< 12
-                        let compressedBufferSize = readBigEndian32At xs o
+                        let compressedBufferSize = parseInt32 xs o
                         let compressionType = int(xs.[o+4])
                         
                         let compressedBuffer = Array.sub xs (o+5) (compressedBufferSize-1)
@@ -95,91 +106,29 @@ module Minecraft =
                         ()
             }
 
-    let rec readPayload (buffer : byte[]) (offset : int) tagId =
-        let mutable o = offset
-        match tagId with
-        
-        // TAG_End
-        |  0 -> (PayloadEnd, o)
-        
-        // TAG_Short
-        |  2 -> (readBigEndianInt16 buffer o |> PayloadShort, o + 2)
-        
-        // TAG_Int
-        |  3 -> (readBigEndianInt32 buffer o |> PayloadInt, o + 4)
-        
-        // TAG_Long
-        |  4 -> (readBigEndianInt64 buffer o |> PayloadLong, o + 8)
-        
-        // TAG_Byte_Array
-        |  7 -> 
-            let count = readBigEndianInt32 buffer o
-            o <- o + 4
-            let xs = Array.sub buffer o count
-            o <- o + count
-            (PayloadByteArray xs, o)
-
-        // TAG_String
-        |  8 ->
-            let length = readBigEndian16At buffer o
-            o <- o + 2
-            let s = Array.sub buffer o length |> Encoding.UTF8.GetString
-            o <- o + length
-            (PayloadString s, o)
-
-        // TAG_List
-        |  9 ->
-            let listItemsTagId = int(buffer.[o])
-            o <- o + 1
-            let count = readBigEndianInt32 buffer o
-            o <- o + 4
-            let xs = Array.zeroCreate count
-            for i = 0 to count-1 do
-                let (x, newOffset) = readPayload buffer o listItemsTagId
-                xs.[i] <- x
-                o <- newOffset
-            (PayloadList xs, o)
-
-        // TAG_Int_Array
-        | 11 -> 
-            let count = readBigEndianInt32 buffer o
-            o <- o + 4
-            let xs = Array.zeroCreate count
-            for i = 0 to count-1 do
-                xs.[i] <- readBigEndianInt32 buffer o
-                o <- o + 4
-            (PayloadIntArray xs, o)
-
-        // TAG_Long_Array
-        | 12 -> 
-            let count = readBigEndianInt32 buffer o
-            o <- o + 4
-            let xs = Array.zeroCreate count
-            for i = 0 to count-1 do
-                xs.[i] <- readBigEndianInt64 buffer o
-                o <- o + 4
-            (PayloadLongArray xs, o)
-
-        // unknown TAG
-        |  _ -> failwith "not implemented"
-
     let parseRawNbtBuffer (x : RawNbtBuffer) =
 
         let buffer = x.Buffer
         File.WriteAllBytes("T:/debug.nbt", buffer)
 
-        let rec parse offset =
+        let rec parse (tagId : int option) doNotParseTagName offset =
 
             // parses list of tags until PayloadEnd is reached
-            let rec parseList o =
-                match parse o with
+            let rec parseList doNotParseTagName o =
+                match parse None doNotParseTagName o with
                 | ({ Payload = PayloadEnd }, newOffset) -> ([], newOffset)
-                | ( tag, o1) -> let (xs, o2) = parseList o1
+                | ( tag, o1) -> let (xs, o2) = parseList doNotParseTagName o1
                                 (tag :: xs, o2)
 
             let mutable o = offset
-            let tagId = int(buffer.[o])
-            o <- o + 1
+
+            let tagId = 
+                match tagId with
+                | Some x -> x
+                | None   -> let x = int(buffer.[o])
+                            o <- o + 1
+                            x
+
             if tagId = 0 then
 
                 // TAG_End
@@ -187,33 +136,103 @@ module Minecraft =
 
             else
 
-                let tagNameLength = readBigEndian16At buffer o
-                o <- o + 2
-                let tagName = Array.sub buffer o tagNameLength |> Encoding.UTF8.GetString
-                o <- o + tagNameLength
-                match tagId with
-                                
-                |  2    // TAG_Short                
-                |  3    // TAG_Int                
-                |  4    // TAG_Long   
-                |  7    // TAG_Byte_Array
-                |  8    // TAG_String  
-                |  9    // TAG_List              
-                | 11    // TAG_Int_Array           
-                | 12 -> // TAG_Long_Array
-                    let (x, newOffset) = readPayload buffer o tagId
-                    ({ Name = tagName; Payload = x }, newOffset)
+                let tagName = 
+                    match doNotParseTagName with
+                    | true  -> ""
+                    | false ->
+                        let (tagName, newOffset) = parseString buffer o
+                        o <- newOffset
+                        tagName
 
+                //printfn "%d %s" tagId tagName
+
+                match tagId with
+                      
+                // TAG_Byte   
+                |  1 ->
+                    ({ Name = tagName; Payload = PayloadByte buffer.[o] }, o + 1)
+
+                // TAG_Short   
+                |  2 ->
+                    let x = parseInt16 buffer o |> PayloadShort
+                    ({ Name = tagName; Payload = x }, o + 2)
+
+                // TAG_Int   
+                |  3 ->
+                    let x = parseInt32 buffer o |> PayloadInt
+                    ({ Name = tagName; Payload = x }, o + 4)
+                
+                // TAG_Long   
+                |  4 ->
+                    let x = parseInt64 buffer o |> PayloadLong
+                    ({ Name = tagName; Payload = x }, o + 8)
+
+                // TAG_Double   
+                |  5 ->
+                    let x = parseFloat32 buffer o |> PayloadFloat
+                    ({ Name = tagName; Payload = x }, o + 4)
+
+                // TAG_Double   
+                |  6 ->
+                    let x = parseFloat64 buffer o |> PayloadDouble
+                    ({ Name = tagName; Payload = x }, o + 8)
+
+                // TAG_Byte_Array
+                |  7 ->
+                    let count = parseInt32 buffer o
+                    o <- o + 4
+                    let xs = Array.sub buffer o count
+                    o <- o + count
+                    ({ Name = tagName; Payload = PayloadByteArray xs }, o)
+
+                // TAG_String  
+                |  8 ->
+                    let (s, newOffset) = parseString buffer o
+                    ({ Name = tagName; Payload = PayloadString s }, newOffset)
+
+                 // TAG_List   
+                |  9 ->
+                    let listItemsTagId = int(buffer.[o])
+                    o <- o + 1
+                    let count = parseInt32 buffer o
+                    o <- o + 4
+                    let xs = Array.zeroCreate count
+                    for i = 0 to count-1 do
+                        let (x, newOffset) = parse (Some listItemsTagId) true o 
+                        xs.[i] <- x.Payload
+                        o <- newOffset
+                    ({ Name = tagName; Payload = PayloadList xs }, o)
+                    
                 // TAG_Compound
                 | 10 ->
-                    let (xs, newOffset) = parseList o
-                    ({ Name = tagName; Payload = PayloadCompound(xs) }, newOffset)
+                    let (xs, newOffset) = parseList false o
+                    ({ Name = tagName; Payload = PayloadCompound xs }, newOffset)
+
+                // TAG_Int_Array 
+                | 11 ->
+                    let count = parseInt32 buffer o
+                    o <- o + 4
+                    let xs = Array.zeroCreate count
+                    for i = 0 to count-1 do
+                        xs.[i] <- parseInt32 buffer o
+                        o <- o + 4
+                    ({ Name = tagName; Payload = PayloadIntArray xs }, o)
+                
+                // TAG_Long_Array
+                | 12 ->
+                    let count = parseInt32 buffer o
+                    o <- o + 4
+                    let xs = Array.zeroCreate count
+                    for i = 0 to count-1 do
+                        xs.[i] <- parseInt64 buffer o
+                        o <- o + 8
+                    ({ Name = tagName; Payload = PayloadLongArray xs }, o)
 
                 // unknown TAG
                 | _  ->
                     failwith "not implemented"
 
-        parse 0
+        parse None false 0
 
 
     let test () =
